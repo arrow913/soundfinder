3abb8b08e88f76a108f41248568beea51e9295ec
Caleb Chaney <calebc347@gmail.com>
2020-09-16 19:31:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,14 @@
 function searchArtists(artist) {
 
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
-
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
 
-
-        var artistName = $("<h1>").text(response.name);
-        var upcomingEvents = $("<h1>").text(response.upcoming_event_count + " upcoming events");
+        var upcomingEvents = $("<h1>").text(response.upcoming_event_count + " Upcoming Events");
         var gotoArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
 
         // Empty the contents, append the new artist content
