91633f25670d5e220e83bf75ca3994d4275002cf
Carson Savage <carsonesavage@gmail.com>
2020-09-19 09:47:25 -0500

    added granim for javascript library

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'diagonal',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
 
 function searchArtists(artist) {
 
