3322a1a414642d925201fb8a12ff002c012c716f
Carson Savage <carsonesavage@gmail.com>
2020-09-21 18:50:16 -0500

    working on granim for background, changed stylings

--- main.js
+++ main.js
@@ -86,3 +86,17 @@ $("#searchButton").on("click", function () {
 });
 
 
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
